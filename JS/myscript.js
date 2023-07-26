28f18c5ad1da78035c422827a180e7cb15100262
StefanoMela <stefano.mela25@gmail.com>
2023-07-26 16:34:41 +0200

    feat: 3rd Snack

--- JS/myscript.js
+++ JS/myscript.js
@@ -26,24 +26,39 @@
 // Dare la possibilità di inserire due parole. Verificare che le due parole abbiano la stessa lunghezza. 
 // Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
 
+// SVOLGIMENTO
 
-const firstWord = prompt("Dimmi una prima parola");
-const secondWord = prompt("Dammene una seconda");
+// const firstWord = prompt("Dimmi una prima parola");
+// const secondWord = prompt("Dammene una seconda");
 
-if (firstWord.length == secondWord.length) {
+// if (firstWord.length == secondWord.length) {
 
-    console.log(firstWord , secondWord);
+//     console.log(firstWord , secondWord);
 
-} else if (firstWord.length > secondWord.length) {
+// } else if (firstWord.length > secondWord.length) {
 
-    console.log(firstWord);
+//     console.log(firstWord);
 
-} else {
-    
-    console.log(secondWord);
-};
+// } else {
 
+//     console.log(secondWord);
+// };
 
 // # TRACCIA 3
+
+// Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. 
+// Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+
+const userArray = [];
+let totalSum = 0;
+
+while (totalSum < 50) {
+    
+    let userNumber = parseInt(prompt("Dammi un numero"));
+    totalSum += userNumber;
+    userArray.push(userNumber);
+    
+};
+console.log(userArray);
 
 // # TRACCIA 4
