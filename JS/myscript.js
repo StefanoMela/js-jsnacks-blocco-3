d53fe54390972b43226e423cca205c255b49b998
StefanoMela <stefano.mela25@gmail.com>
2023-07-26 16:02:03 +0200

    feat: 2nd Snack

--- JS/myscript.js
+++ JS/myscript.js
@@ -1,5 +1,3 @@
-// console.log("JS OK");
-
 // # TRACCIA 1
 
 // Chiedi all'utente un primo numero, poi un secondo e genera un numero casuale compreso tra i due.
@@ -8,23 +6,43 @@
 
 // SVOLGIMENTO
 
-const firstNumber = parseInt(prompt("Dimmi un primo numero"));
-const secondNumber = parseInt(prompt("Dimmi un secondo numero"));
+// const firstNumber = parseInt(prompt("Dimmi un primo numero"));
+// const secondNumber = parseInt(prompt("Dimmi un secondo numero"));
 
-if (firstNumber < secondNumber) {
+// if (firstNumber < secondNumber) {
     
-    let randomNumber = Math.floor(Math.random() * (secondNumber - firstNumber)) + firstNumber;
-    console.log(randomNumber);
+//     let randomNumber = Math.floor(Math.random() * (secondNumber - firstNumber)) + firstNumber;
+//     console.log(randomNumber);
     
-} else {
+// } else {
 
-    let randomSecond = Math.floor(Math.random() * (firstNumber - secondNumber)) + secondNumber;
+//     let randomSecond = Math.floor(Math.random() * (firstNumber - secondNumber)) + secondNumber;
 
-    console.log(randomSecond);
-};
-
+//     console.log(randomSecond);
+// };
 
 // # TRACCIA 2
+
+// Dare la possibilità di inserire due parole. Verificare che le due parole abbiano la stessa lunghezza. 
+// Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
+
+
+const firstWord = prompt("Dimmi una prima parola");
+const secondWord = prompt("Dammene una seconda");
+
+if (firstWord.length == secondWord.length) {
+
+    console.log(firstWord , secondWord);
+
+} else if (firstWord.length > secondWord.length) {
+
+    console.log(firstWord);
+
+} else {
+    
+    console.log(secondWord);
+};
+
 
 // # TRACCIA 3
 
