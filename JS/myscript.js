7ffbe14c02ac170698559d4f00481c3cf0f09d76
StefanoMela <stefano.mela25@gmail.com>
2023-07-26 15:46:57 +0200

    feat: 1st snack

--- JS/myscript.js
+++ JS/myscript.js
@@ -1,1 +1,31 @@
-console.log("JS OK");
+// console.log("JS OK");
+
+// # TRACCIA 1
+
+// Chiedi all'utente un primo numero, poi un secondo e genera un numero casuale compreso tra i due.
+
+// generare random Numb fra due dati math.floor(math.random() )
+
+// SVOLGIMENTO
+
+const firstNumber = parseInt(prompt("Dimmi un primo numero"));
+const secondNumber = parseInt(prompt("Dimmi un secondo numero"));
+
+if (firstNumber < secondNumber) {
+    
+    let randomNumber = Math.floor(Math.random() * (secondNumber - firstNumber)) + firstNumber;
+    console.log(randomNumber);
+    
+} else {
+
+    let randomSecond = Math.floor(Math.random() * (firstNumber - secondNumber)) + secondNumber;
+
+    console.log(randomSecond);
+};
+
+
+// # TRACCIA 2
+
+// # TRACCIA 3
+
+// # TRACCIA 4
